2ad4ed345deb0ef10029ca8215282a7c8d640f64
saki <sakihet@gmail.com>
2025-11-18 10:17:38 +0900

    add SelectedBy, Form

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ struct Args {
 
 #[derive(Clone, Debug, ValueEnum)]
 enum SortBy {
+    Cost,
+    SelectedBy,
+    Form,
     Points,
-    Price,
 }
 
 impl Default for SortBy {
@@ -42,50 +44,70 @@ async fn main() {
     let args = Args::parse();
 
     match args.commands {
-        Commands::Player { sort } => {
-            println!("show players");
-            match fetch_fpl_data().await {
-                Ok(data) => {
-                    if let Some(elements) = data["elements"].as_array() {
-                        println!("Found {} players", elements.len());
-                        let mut players: Vec<_> = elements
-                            .iter()
-                            .filter_map(|player| {
-                                let price = player["now_cost"].as_u64()?;
-                                let points = player["total_points"].as_u64()?;
-                                Some((price, points, player))
-                            })
-                            .collect();
+        Commands::Player { sort } => match fetch_fpl_data().await {
+            Ok(data) => {
+                if let Some(elements) = data["elements"].as_array() {
+                    let mut players: Vec<_> = elements
+                        .iter()
+                        .filter_map(|player| {
+                            let id = player["id"].as_u64()?;
+                            let cost = player["now_cost"].as_u64()?;
+                            let selected_by = player["selected_by_percent"]
+                                .as_str()?
+                                .parse::<f64>()
+                                .ok()?;
+                            let form = player["form"].as_str()?.parse::<f64>().ok()?;
+                            let points = player["total_points"].as_u64()?;
+                            Some((id, cost, selected_by, form, points, player))
+                        })
+                        .collect();
 
-                        match sort {
-                            SortBy::Price => players.sort_by(|a, b| b.0.cmp(&a.0)),
-                            SortBy::Points => players.sort_by(|a, b| b.1.cmp(&a.1)),
+                    match sort {
+                        SortBy::Cost => players.sort_by(|a, b| b.1.cmp(&a.1)),
+                        SortBy::SelectedBy => {
+                            players.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap())
                         }
+                        SortBy::Form => players.sort_by(|a, b| b.3.partial_cmp(&a.3).unwrap()),
+                        SortBy::Points => players.sort_by(|a, b| b.4.cmp(&a.4)),
+                    }
 
-                        println!(
-                            "{:<20} {:<15} {:<8} {:<8}",
-                            "Name", "Team", "Price", "Points"
-                        );
-                        for (_price, points, player) in players.iter().take(20) {
-                            if let (Some(web_name), Some(team_code), Some(price)) = (
-                                player["web_name"].as_str(),
-                                player["team_code"].as_u64(),
-                                player["now_cost"].as_u64(),
-                            ) {
-                                let price_formatted = format!("{:.1}", price as f64 / 10.0);
-                                println!(
-                                    "{:<20} {:<15} {:<8} {:<8}",
-                                    web_name, team_code, price_formatted, points
-                                );
-                            }
+                    println!(
+                        "{:<4} {:<20} {:<15} {:<8} {:<8} {:<8} {:<8}",
+                        "ID", "Name", "Team", "Cost", "Selected", "Form", "Points"
+                    );
+                    for (_id, _cost, _selected_by, _form, _points, player) in
+                        players.iter().take(20)
+                    {
+                        if let (
+                            Some(id),
+                            Some(web_name),
+                            Some(team_code),
+                            Some(price),
+                            Some(selected_by),
+                            Some(form),
+                            Some(points),
+                        ) = (
+                            player["id"].as_u64(),
+                            player["web_name"].as_str(),
+                            player["team_code"].as_u64(),
+                            player["now_cost"].as_u64(),
+                            player["selected_by_percent"].as_str(),
+                            player["form"].as_str(),
+                            player["total_points"].as_u64(),
+                        ) {
+                            let price_formatted = format!("{:.1}", price as f64 / 10.0);
+                            println!(
+                                "{:<4} {:<20} {:<15} {:<8} {:<8} {:<8} {:<8}",
+                                id, web_name, team_code, price_formatted, selected_by, form, points
+                            );
                         }
                     }
                 }
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                }
             }
-        }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        },
         Commands::Team {} => {
             println!("show teams");
             match fetch_fpl_data().await {
